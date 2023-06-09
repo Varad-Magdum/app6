8fea01fb0c2618e714f67f8a0853751550a30fea
Varad-Magdum <varadmagdum33@gmail.com>
2023-06-09 15:26:08 +0530

    done

---
+++ src/Component/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='contact'>
+      <h2>
+        Contact Me at varadmagdum33@gmail.com
+      </h2>
+
+    </div>
+  )
+}
+
+export default Contact
